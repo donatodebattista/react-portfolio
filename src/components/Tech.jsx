7fe0618d2ac40793b784e64052b93aed773a8afa
Donato De Battista <doni.db@hotmail.com>
2025-09-08 19:01:42 -0300

    fix: marquee icons size

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,12 @@
 export default function Tech({ tech }) {
-  const Icon = tech.icon
+  const Icon = tech.icon;
   return (
     <div className='flex flex-row mx-12 items-center'>
-      <span className="w-8 flex items-center justify-center">
-        <Icon />
+      <span
+        className="w-8 h-8 flex items-center justify-center"
+        aria-hidden="true"
+      >
+        <Icon className="w-full h-full"/>
       </span>
       <p className='text-whiteText ml-2 pointer'>{tech.name}</p>
     </div>
