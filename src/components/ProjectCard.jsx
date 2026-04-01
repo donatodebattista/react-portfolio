7878f667e56a467a7608c9bf788ee8bb8d224e63
donatodebattista <doni.db@hotmail.com>
2026-04-01 01:36:25 -0300

    change flybondi project image

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,13 +2,13 @@ import { FaCode } from "react-icons/fa";
 import { MdArrowOutward } from "react-icons/md";
 import ButtonLink from "./ButtonLink";
 
-export default function ProjectCard({ title, imageUrl, repoUrl, siteUrl, isDeployed, isRepoDisabled=false, isSiteDisabled=false }) {
+export default function ProjectCard({ title, imageUrl, repoUrl, siteUrl, isDeployed, isRepoDisabled = false, isSiteDisabled = false }) {
   return (
     <div className="bg-[#1717179c] rounded-2xl shadow-lg overflow-hidden group relative transition-transform duration-300 border  border-[#62626227]">
       <img
         src={imageUrl}
         alt={`Vista previa de ${title}`}
-        className="w-full h-48 object-cover"
+        className="w-full aspect-video object-cover object-center"
       />
 
       <div className="p-4 flex justify-between items-start">
